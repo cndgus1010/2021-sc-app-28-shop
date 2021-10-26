aef589cc9f89ffbb9a72b5a585958415fea70b74
cndggus1010 <chung1838@namver.com>
2021-10-26 14:57:53 +0900

    first commit

---
+++ models/index.js
@@ -0,0 +1,37 @@
+const fs = require('fs-extra');
+const path = require('path');
+const Sequelize = require('sequelize');
+const config = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: process.env.DIALECT,
+};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+fs.readdirSync(__dirname)
+  .filter((file) => file !== 'index.js')
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
