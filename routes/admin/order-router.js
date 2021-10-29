fa12ff5cfdda31e716003ffcdc97910c39418aa1
cndggus1010 <chung1838@namver.com>
2021-10-29 15:51:58 +0900

    카테고리 제외 퍼블리싱 완료

--- routes/admin/order-router.js
+++ routes/admin/order-router.js
@@ -1,26 +1,30 @@
-const path = require('path')
-const express = require('express')
-const router = express.Router()
-const { error } = require('../../modules/util')
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const { error } = require('../../modules/util');
 
 router.get('/', (req, res, next) => {
   if (req.query.type === 'update') {
-    res.render('admin/order/order-form', { css: 'admin-order' })
+    res.render('admin/order/order-form', { css: 'admin-order' });
   } else {
-    res.render('admin/order/order-list', { css: 'admin-order' })
+    res.render('admin/order/order-list', { css: 'admin-order' });
   }
-})
+});
+
+router.get('/:id', (req, res, next) => {
+  res.render('admin/order/order-form', { css: 'admin-order' });
+});
 
 router.post('/', (req, res, next) => {
-  res.send('/admin/order:POST')
-})
+  res.send('/admin/order:POST');
+});
 
 router.put('/', (req, res, next) => {
-  res.send('/admin/order:PUT')
-})
+  res.send('/admin/order:PUT');
+});
 
 router.delete('/', (req, res, next) => {
-  res.send('/admin/order:DELETE')
-})
+  res.send('/admin/order:DELETE');
+});
 
-module.exports = { name: '/order', router }
+module.exports = { name: '/order', router };
