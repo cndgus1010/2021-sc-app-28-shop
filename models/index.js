476868d1311bcb7013fbbbbb83204626c8fd4e09
cndggus1010 <chung1838@namver.com>
2021-11-05 13:26:43 +0900

    seed 처리 오류

--- models/index.js
+++ models/index.js
@@ -7,8 +7,8 @@ const config = require('../config/config')[env];
 config.timezone = '+09:00';
 const db = {};
 
-Sequelize.prototype.getWhere = ({ field, search }) => {
-  let where = search ? { [field]: { [Op.like]: '%' + search + '%' } } : null;
+Sequelize.prototype.getWhere = function ({ field, search }) {
+  let where = search ? { [field]: { [Op.like]: '%' + search + '%' } } : {};
   if (field === 'tel' && search !== '') {
     where = this.where(this.fn('replace', this.col('tel'), '-', ''), {
       [Op.like]: '%' + search.replace(/-/g, '') + '%',
