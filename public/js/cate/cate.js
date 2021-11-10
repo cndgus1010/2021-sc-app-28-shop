a05c9d35bd3867a004d9d624d269fdd481750f74
cndggus1010 <chung1838@namver.com>
2021-11-10 12:15:19 +0900

    cate.json cate.js

---
+++ public/js/cate/cate.js
@@ -0,0 +1,27 @@
+var core = {};
+var plugins = ['contextmenu', 'dnd', 'search', 'state', 'wholerow', 'changed'];
+
+core.themes = {
+  variant: true,
+  striped: true,
+};
+
+core.check_callback = true;
+
+core.data = {
+  url: function (node) {
+    return '/api/tree';
+  },
+  data: function (node) {
+    return { id: node.id };
+  },
+};
+
+function onChangedTree(e, data) {
+  console.log(data.changed.selected); // newly selected
+  console.log(data.changed.deselected); // newly deselected
+}
+
+$('#jstreeWrap')
+  .jstree({ core: core, plugins: plugins })
+  .on('changed.jstree', onChangedTree);
