692b7c04596e973bea3b774199ad8eaf492610dd
cndggus1010 <chung1838@namver.com>
2021-11-13 23:39:47 +0900

    11-13 복습 시작

--- models/index.js
+++ models/index.js
@@ -8,13 +8,14 @@ config.timezone = '+09:00';
 const db = {};
 
 Sequelize.prototype.getWhere = function ({ field, search }) {
-  let where = search ? { [field]: { [Op.like]: '%' + search + '%' } } : {};
-  if (field === 'tel' && search !== '') {
+  let where = {};
+  if (field === 'tel') {
+    // 회원검색
     where = this.where(this.fn('replace', this.col('tel'), '-', ''), {
       [Op.like]: '%' + search.replace(/-/g, '') + '%',
     });
-  }
-  if (field === 'addrRoad' && search !== '') {
+  } else if (field === 'addrRoad') {
+    // 회원검색
     where = {
       [Op.or]: {
         addrPost: { [Op.like]: '%' + search + '%' },
@@ -24,6 +25,8 @@ Sequelize.prototype.getWhere = function ({ field, search }) {
         addrDetail: { [Op.like]: '%' + search + '%' },
       },
     };
+  } else {
+    where = search ? { [field]: { [Op.like]: '%' + search + '%' } } : {};
   }
   return where;
 };
--- routes/admin/prd-router.js
+++ routes/admin/prd-router.js
@@ -4,17 +4,26 @@ const router = express.Router();
 const escape = require('escape-html');
 const createError = require('http-errors');
 const { error } = require('../../modules/util');
-const Product = require('../../models/Product');
+const { Product, ProductFile, CateProduct } = require('../../models');
+const uploader = require('../../middlewares/multer-mw');
+const afterUploader = require('../../middlewares/after-multer-mw');
+const { moveFile } = require('../../modules/util');
+const queries = require('../../middlewares/query-mw');
 
-router.get('/', (req, res, next) => {
+router.get('/', queries(), (req, res, next) => {
   if (req.query.type === 'create') {
     res.render('admin/prd/prd-form');
   } else next();
 });
 
-router.get('/', async (req, res, next) => {
+router.get('/', queries(), async (req, res, next) => {
   try {
-    res.render('admin/prd/prd-list');
+    const { lists, pager, totalRecord } = await Product.getLists(
+      req.query,
+      ProductFile
+    );
+    // res.json({ lists, pager, totalRecord });
+    res.render('admin/prd/prd-list', { lists, pager, totalRecord });
   } catch (err) {
     next(createError(err));
   }
@@ -28,16 +37,36 @@ router.get('/:id', async (req, res, next) => {
   }
 });
 
-router.post('/', async (req, res, next) => {
-  try {
-    res.json({ body: req.body, content: escape(req.body.content) });
-    // req.body.content = escape(req.body.content);
-    // await Product.create(req.body)
-    // res.redirect('/admin/prd');
-  } catch (err) {
-    next(createError(err));
+router.post(
+  '/',
+  uploader.fields([{ name: 'img' }, { name: 'detail' }]),
+  afterUploader(['img', 'detail']),
+  queries('body'),
+  async (req, res, next) => {
+    try {
+      if (req.body.type === 'update') {
+        // await Board.update(req.body, { where: { id: req.body.id } });
+        // req.files.forEach((file) => (file.board_id = req.body.id));
+        // const files = await BoardFile.bulkCreate(req.files);
+        // res.json({ file: req.files, req: req.body, locals: res.locals });
+        // res.redirect(res.locals.goList);
+      } else {
+        req.body.content = escape(req.body.content);
+        const product = await Product.create(req.body);
+        req.files.forEach((file) => (file.prd_id = product.id));
+        await ProductFile.bulkCreate(req.files);
+        const catePrd = req.body.cate.split(',').map((cate) => ({
+          cate_id: cate,
+          prd_id: product.id,
+        }));
+        await CateProduct.bulkCreate(catePrd);
+        res.redirect('/admin/prd');
+      }
+    } catch (err) {
+      next(createError(err));
+    }
   }
-});
+);
 
 router.put('/', async (req, res, next) => {
   try {
@@ -47,9 +76,27 @@ router.put('/', async (req, res, next) => {
   }
 });
 
-router.delete('/', async (req, res, next) => {
+router.put('/status', queries('body'), async (req, res, next) => {
   try {
-    res.redirect('/admin/prd');
+    const { status, id } = req.body;
+    await Product.update({ status }, { where: { id } });
+    res.redirect(res.locals.goList);
+  } catch (err) {
+    next(createError(err));
+  }
+});
+
+router.delete('/', queries('body'), async (req, res, next) => {
+  try {
+    const { id } = req.body;
+    await Product.destroy({ where: { id } });
+    const files = await ProductFile.findAll({
+      attributes: ['saveName'],
+      where: { prd_id: id },
+    });
+    for (let { saveName } of files) await moveFile(saveName);
+    await ProductFile.destroy({ where: { prd_id: id } });
+    res.redirect(res.locals.goList);
   } catch (err) {
     next(createError(err));
   }
