42b369fbccc2bebab8b5fcce7a011239aa1c3bac
cndggus1010 <chung1838@namver.com>
2021-12-09 15:22:49 +0900

    seed 오류

--- modules/sequelize-init.js
+++ modules/sequelize-init.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize) => {
-  sequelize
+  sequelize 
     .sync({ force: false }) // 실제DBMNS랑 연동시켜줘
     .then(() => console.log('Sequelize Start!'))
     .catch((err) => console.log('Sequelize Error => ', err));
