4c9953b9a1c64d1a0c557bedb44d6af205e3bce5
cndggus1010 <chung1838@namver.com>
2021-12-14 16:58:50 +0900

    메일 작업중

--- modules/util.js
+++ modules/util.js
@@ -186,6 +186,18 @@ function findAllId(_obj, arr) {
   return arr;
 }
 
+// _obj의 자식들의 값을 리턴하는 재귀함수2
+function findAllId2(_obj, arr) {
+  if (_obj.children) {
+    for (let v of _obj.children) {
+      findAllId2(v, arr);
+    }
+  }
+  arr.push({ id: _obj.id, name: _obj.text });
+  return arr;
+}
+
+
 module.exports = {
   location,
   cutTail,
@@ -206,5 +218,6 @@ module.exports = {
   findLastId,
   findChildId,
   findAllId,
+  findAllId2,
   findObj,
 };
