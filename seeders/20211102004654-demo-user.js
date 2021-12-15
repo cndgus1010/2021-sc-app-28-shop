4a28476a4653df540cc90abc5eff72894e0d4c1d
cndggus1010 <chung1838@namver.com>
2021-12-15 17:00:49 +0900

    public 파일 교체

---
+++ seeders/20211102004654-demo-user.js
@@ -0,0 +1,60 @@
+const numeral = require('numeral');
+const bcrypt = require('bcrypt');
+const { BCRYPT_SALT: salt, BCRYPT_ROUND: round } = process.env;
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const insertUsers = [];
+    insertUsers.push({
+      userid: 'chung0',
+      userpw: await bcrypt.hash('000000' + salt, Number(round)),
+      username: '최고관리자',
+      email: 'chung0@chung0.com',
+      tel: '010-9367-5559',
+      addrPost: '12345',
+      addrRoad: '인천시 서구 봉오대로',
+      addrJibun: '인천시 서구 가정동',
+      addrComment: '(가정동)',
+      addrDetail: '7층',
+      status: '9',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    insertUsers.push({
+      userid: 'demoUser',
+      userpw: await bcrypt.hash('111111' + salt, Number(round)),
+      username: '데모관리자',
+      email: 'demo@demo.com',
+      tel: '010-3333-8888',
+      addrPost: '12345',
+      addrRoad: '서울시 마포구 노고산로',
+      addrJibun: '서울시 마포구 창천동',
+      addrComment: '(창천동)',
+      addrDetail: '7층',
+      status: '7',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    for (let i = 0; i < 45; i++) {
+      insertUsers.push({
+        userid: 'test' + i,
+        userpw: await bcrypt.hash('222222' + salt, Number(round)),
+        username: '테스트유저' + i,
+        email: 'test' + i + '@test.com',
+        tel: `010-7777-${numeral(i).format('0000')}`,
+        addrPost: String(10000 + i),
+        addrRoad: '서울시 마포구 노고산로',
+        addrJibun: '서울시 마포구 창천동',
+        addrComment: '(창천동)',
+        addrDetail: 1 + i + '층',
+        status: '2',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+    await queryInterface.bulkInsert('user', insertUsers);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('user', null, {});
+  },
+};
